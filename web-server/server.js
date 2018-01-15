7ac361f18d2b09fa02bb1e3620224f07e582dd56
shakkeel1330 <shakkeelahammed@gmail.com>
2018-01-15 17:41:09 +0530

    Added sessions and cookies.

--- web-server/server.js
+++ web-server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var moment = require('moment');
 var passport = require('passport')
 var session = require('express-session')
 var appController = require('./appointmentAppController.js')
 var gapi = require('./quickstart.js')
 var cookieParser = require('cookie-parser');
-var session = require('express-session');
 var app = express();
 var path = require('path')
 app.use(express.static(path.join(__dirname, 'Public')));
@@ -13,107 +13,120 @@ app.use(express.static(path.join(__dirname, 'Public')));
 require('./config/passport')(passport); // pass passport for configuration
 
 // required for passport
-// app.use(session({ secret: 'secretkeysecret' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
+
 
 app.use(express.static(__dirname + 'Public'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '50mb' }));
+app.use(session({ secret: 'secretkeysecret', key: 'user_sid', cookie: { expires: 600000 }, resave: false, saveUninitialized: false })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+// app.use(session({
+//     key: 'user_sid',
+//     secret: 'somerandonstuffs',
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//         expires: 600000
+//     }
+// }));
 
-// app.get('/login', (req, res) => {
-//     res.render('login.html');
-
-//     });
+// // This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
+// // This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
+// app.use((req, res, next) => {
+//     if (req.cookies.user_sid && !req.session.user) {
+//         res.clearCookie('user_sid');
+//     }
+//     next();
 // });
 
-// app.get('/', function(req, res) {
-//     res.render('index.html');
+
+
+// var sessionChecker = (req, res, next) => {
+//     if (req.session.user && req.cookies.user_sid) {
+//         res.redirect('/members');
+//     } else {
+//         next();
+//     }
+// };
+
+
+// app.get('/members', sessionChecker, (req, res) => {
+//     res.redirect('/login');
+// });
+
+
+app.get('/#/', (req, res) => {
+    res.render('index.html');
+    res.clearCookie('user_sid');
+    req.session.destroy();
+})
+
+app.get('/index2',checkAuthentication,function(req,res){
+    res.render("index2.html");
+});
+
+// app.get('/index2', (req, res) => {
+//     res.render('index2.html');
 // })
 
-// app.get('/members',sessionChecker,)
-
-app.use(session({
-    key: 'user_sid',
-    secret: 'somerandonstuffs',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
-}));
-
-// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
-// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
-app.use((req, res, next) => {
-    if (req.cookies.user_sid && !req.session.user) {
-        res.clearCookie('user_sid');
-    }
-    next();
-});
-
-
-
-var sessionChecker = (req, res, next) => {
-    if (req.session.user && req.cookies.user_sid) {
-        res.redirect('/members');
-    } else {
-        next();
-    }
-};
-
-
-app.get('/members', sessionChecker, (req, res) => {
-    res.redirect('/login');
-});
-
-
 app.get('/logout', (req, res) => {
-    if (req.session.user && req.cookies.user_sid) {
+    if (req.cookies.user_sid) {
         res.clearCookie('user_sid');
+        req.session.destroy();
         res.redirect('/');
     } else {
-        res.redirect('/login');
+        res.redirect('/');
+        req.session.destroy();
+        res.clearCookie('user_sid');
     }
 });
 
 
-// app.get('/login', function(req, res) {
-//     res.render('login.html');
-// });
 
 
 
 app.get('/appointmentInfo', (req, res) => {
 
 
-    appController.viewAll(function(err, result) {
-        if (err) {
-            console.log(err)
-        }
+    // appController.viewAll(function(err, result) {
+    //     if (err) {
+    //         console.log(err)
+    //     }
 
-        res.json(result);
+    // res.json(result);
+    // console.log(req.body);
+    var startTime = moment().format();
+    var endTime = moment().month(1).format();
+    // gapi.authorize(JSON.parse(gapi.content), startTime, endTime, gapi.availability);
+    gapi.availability(JSON.parse(gapi.content), startTime, endTime, function(freeBusy) {
+        // console.log(freeBusy);
+        res.json(freeBusy);
+        console.log(req);
     });
+
 });
 
 app.post('/newAppointment', (req, res) => {
 
-    obj = { id: parseInt(req.body.id), start: new Date(req.body.start), end: new Date(req.body.end), doctorid: parseInt(req.body.doctorid), patientid: parseInt(req.body.patientid), schedule: req.body.schedule, status: req.body.status, venue: req.body.venue };
-    console.log(obj.end);
-    appController.addAppointment(obj, function(err, result) {
-        if (err) {
-            res.status(400)
-                .send({
-                    message: "Invalid input",
-                    status: res.status
-                })
-        }
-        res.json(result);
-        gapi.authorize(JSON.parse(gapi.content), obj.start, obj.end, gapi.bookSlot);
-        console.log("Appointment saved");
-
-    })
+    // obj = { id: parseInt(req.body.id), start: new Date(req.body.start), end: new Date(req.body.end), doctorid: parseInt(req.body.doctorid), patientid: parseInt(req.body.patientid), schedule: req.body.schedule, status: req.body.status, venue: req.body.venue };
+    // console.log(obj.end);
+    // appController.addAppointment(obj, function(err, result) {
+    //     if (err) {
+    //         res.status(400)
+    //             .send({
+    //                 message: "Invalid input",
+    //                 status: res.status
+    //             })
+    //     }
+    //     res.json(result);
+    // gapi.authorize(JSON.parse(gapi.content), obj.start, obj.end, gapi.bookSlot);
+    // console.log("Appointment saved");
+    // console.log(req.body.end);
+    // console.log(req.body.start);
+    gapi.bookSlot(JSON.parse(gapi.content), req.body.start, req.body.end);
+    //alert('Appointment confirmed')
 
 });
 
@@ -160,8 +173,22 @@ app.get('/auth/google/callback',
         failureRedirect: '/'
     }));
 
+function checkAuthentication(req,res,next){
+    if(req.isAuthenticated()){
+        //if user is logged in, req.isAuthenticated() will return true 
+        next();
+    } else{
+        res.redirect("/");
+    }
+}
 
 
 app.listen(8080, () => {
     console.log("Server is up on port 8080");
 });
+
+
+// $.get( "appointmentInfo", function( data ) {
+//   $( ".result" ).html( data ); // insert fullCalendar function here.
+//   alert( "Load was performed." );
+// });
